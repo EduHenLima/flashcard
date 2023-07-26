e8509a14af03c3a788bcd72593654f1a4535f1a5
Eduardo Lim <eduardo.lima@rentcars.com>
2023-07-25 22:05:09 -0300

    Start project

---
+++ src/cards/createCards.py
@@ -0,0 +1,11 @@
+import json
+
+def create(event, context):
+    abc = (process.env);
+    return abc
+    # body = {
+    #     "message":  ,
+    #     "input": event,
+    # }
+    #
+    # return {"statusCode": 200, "body": json.dumps(body)}
