10e6fd05df78d3f31dc08d087b0c3896c99d1bee
Eduardo Lim <eduardo.lima@rentcars.com>
2023-08-05 17:29:22 -0300

    Pong

--- src/API/ping.py
+++ src/API/ping.py
@@ -6,6 +6,6 @@ def pingDatabase(event, context):
     ping = (get_connection().ping(reconnect=False, attempts=1, delay=0))
 
     if ping is None:
-        return "Connection with Success"
+        return "Pong"
     else:
         return "The connection is refused"
