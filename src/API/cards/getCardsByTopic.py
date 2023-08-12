5574d15a36808698233cee3ac06b50152c40d4b7
Eduardo Lim <eduardo.lima@rentcars.com>
2023-08-12 17:17:50 -0300

    Resolved routes

--- src/API/cards/getCardsByTopic.py
+++ src/API/cards/getCardsByTopic.py
@@ -1,19 +1,13 @@
 import json
 
+from src.API.helpers.alchemyEncoder import AlchemyEncoder
 from src.Model.Base.database import get_session
 from src.Model.cards import Cards
 
 
 def get_cards_by_topic(req, context):
-    req = json.loads(req['body'])
+    topics = get_session().query(Cards).filter(Cards.id_assunto == req['pathParameters']['id_topic'])
+    result = topics.all()
 
-    topics = get_session().query(Cards).filter(Cards.id_assunto == req['id_assunto'])
-    for topic in topics:
-        print(vars(topic))
+    return json.dumps(result, cls=AlchemyEncoder)
 
-    body = {
-        "message": "Success!",
-        "input": topics,
-    }
-
-    return json.dumps({"statusCode": 200, "body": body})
